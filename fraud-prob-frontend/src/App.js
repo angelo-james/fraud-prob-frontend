7eeaee80b858b9abe70b39a367ed03ab42f17da6
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-02 11:34:01 -0700

    installed bootstrap and axios

--- fraud-prob-frontend/src/App.js
+++ fraud-prob-frontend/src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FraudTable from './Components/FraudTable';
 import './App.css';
 
 class App extends Component {
+  state = {
+    events: [{
+        name: 'Event 1',
+        userId: 1,
+        pordFraud: 0.25,
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'},
+      {
+        name: 'Event 2',
+        userId: 2,
+        pordFraud: 0.5,
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'},
+      {
+        name: 'Event 3',
+        userId: 3,
+        pordFraud: 0.75,
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'},
+      {
+        name: 'Event 4',
+        userId: 4,
+        pordFraud: 100,
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'}
+    ]
+  }
   render() {
     return (
-      <div className="App">
+      <div>
+        <h1>Fraud Table</h1>
+        <FraudTable/>
+      </div>
+      
+      
+    );
+  }
+}
+
+export default App;
+
+
+
+{/* <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -20,9 +57,4 @@ class App extends Component {
             Learn React
           </a>
         </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+      </div> */}
---
+++ fraud-prob-frontend/src/Components/FraudTable.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FraudTable extends Component {
+  render() {
+    return (
+      <div>
+        This is the Fraud Table
+      </div>
+    )
+  }
+}
+
+export default FraudTable;
