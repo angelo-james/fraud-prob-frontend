ec27c051bdeff40fb063d044a54d4dd8dede3baa
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-02 19:31:39 -0700

    hitting endpoint on dynomo db taking array of object and rendering onto the view

--- fraud-prob-frontend/src/App.js
+++ fraud-prob-frontend/src/App.js
@@ -6,50 +6,55 @@ import axios from 'axios';
 class App extends Component {
   state = {
     events: [{
-        name: 'Event 1',
-        userId: 1,
-        probFraud: 0.25,
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'},
-      {
-        name: 'Event 2',
-        userId: 2,
-        probFraud: 0.5,
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'},
-      {
-        name: 'Event 3',
-        userId: 3,
-        probFraud: 0.75,
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'},
-      {
-        name: 'Event 4',
-        userId: 4,
-        probFraud: 100,
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'}
+      name: 'Event 1',
+      userId: 1,
+      probFraud: 0.25,
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea libero necessitatibus expedita possimus molestiae dicta hic laboriosam deleniti laudantium, cumque ducimus quibusdam eius explicabo, optio voluptatibus saepe quis quisquam adipisci asperiores. Quis modi nihil ex facilis vitae quo quibusdam incidunt velit, quisquam esse possimus repellat debitis minus excepturi. In, magnam."},
+    {
+      name: 'Event 2',
+      userId: 2,
+      probFraud: 0.49,
+      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'},
+    {
+      name: 'Event 3',
+      userId: 3,
+      probFraud: 0.09,
+      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'},
+    {
+      name: 'Event 4',
+      userId: 4,
+      probFraud: 0.99,
+      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi?'}
     ]
   }
-
+  componentDidMount() {
+    this.getData()
+  }
   getData = () => {
-    axios.get(/*insert endpoint here*/)
-    .then( events => { this.setState({ events }) });
+    axios.get('http://52.91.65.129:3333/predict')
+    .then( events => { this.setState({ events: events.data })
+  });
   }
 
   render() {
     let { events } = this.state
     return (
       <div className="text-center">
-        <h1>Fraud Table</h1>
-
-        <div className="container">
-          <FraudTable events={ this.state.events }/>
+        <div className="row text-center border">
+          <h1 className="navbar-brand w-50">
+            Eventbrite Fraud Table
+          </h1>
         </div>
 
-        <div className="row">
-        {events.map(event => (<FraudTable
-          key={ event.userId }
+        <div className="row mt-3">
+        {events && events.map(event => (<FraudTable
+          key={ event.object_id }
           name={ event.name } 
-          userId={ event.userId } 
-          probFraud={ event.probFraud } 
+          object_id={ event.object_id } 
+          fraud_proba={ event.fraud_proba } 
           description={ event.description }
+          country={ event.country }
+          currency={ event.currency }
           />))}
         </div>
 
@@ -59,3 +64,4 @@ class App extends Component {
 }
 
 export default App;
+
--- fraud-prob-frontend/src/Components/FraudTable.js
+++ fraud-prob-frontend/src/Components/FraudTable.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react'
 
-const FraudTable = (props) => (
-  <div className="container">
-    <h3>{props.name}</h3>
-    <p>{props.description}</p>
-    <div>
-      <label>{props.probFraud}</label>
-    </div>
-  </div>
-)
+const labelColor = (num) => {
+  if ((num*=100) < 0.05) {
+    return "alert alert-success col-2";
+  } else if ((num*=100) <= 0.09) {
+    return "alert alert-warning col-2";
+  } else if ((num*=100) > 0.09) {
+    return "alert alert-danger col-2"
+  }
+}
+
+class FraudTable extends Component{
+  state = {
+    open: false
+  }
+  
+  handleToggle = () => {
+    this.setState({ open: !this.state.open })
+  }
+
+  // <div className="container border mt-3 mb-2">
+  render() {
+    return(
+      <div className = "container accordion mb-2" id = "accordionExample" >
+        <div className="card">
+          <div className="card-header alert alert-dark" id="headingOne">
+            <h5 className="mb-0 my-auto">
+              <button onClick={ this.handleToggle } className="btn btn-primary text-center mb-2" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <div className="col-4 mx-auto float-left">
+                  { this.props.name }
+                </div>
+              </button>
+              <div className="container max-width">
+                <label className={ labelColor(this.props.fraud_proba) }>{ `${ this.props.fraud_proba }` }</label>
+              </div>
+            </h5>
+          </div>
+
+          <div id="collapseOne" className={`collapse ${ this.state.open ? 'show' : 'hide' }` } aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div className="card-body border-bottom">
+              <p>Object ID: { this.props.object_id }</p>
+              <p>Country: { this.props.country }</p>
+              <p>Currency: { this.props.currency }</p>
+              <h2 className="text-center">Description</h2>
+              <p>{this.props.description}</p>
+            </div>
+          </div>
+        </div>
+     </div>
+  )}
+
+
+    // <div className="row">
+    //   <h3 className="col-2 m-auto">{props.name}</h3>
+    //   <p className="col-2 m-auto">{props.description}</p>
+    //   <div className="container">
+    //     <label className={ labelColor(props.fraud_proba) }>{ props.fraud_proba }</label>
+    //   </div>
+    // </div>
+  // </div>
+    }
 
 export default FraudTable;
